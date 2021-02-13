3245259f07ef8df0f922aeb13a8fd0e6bcabea4a
Sli5m <rogersbag@gmail.com>
2021-02-13 15:11:12 +0300

    Some more changes to the matplottest file

--- Analysis packages/matplottest.py
+++ Analysis packages/matplottest.py
@@ -1,7 +1,7 @@
 # import matplotlib
 # matplotlib.use('Agg')  # no UI backed
 import matplotlib.pyplot as plt    # Import pyplot as a convention
-
+import numpy as np
 plt.plot([2.3, 3.6, 7.8], [4.2, 1.7, 7.4])    # Plot some co-ordinates
 plt.show()
 # plt.savefig('testfig.png') # Saves the figure in an image
@@ -34,3 +34,47 @@ plt.yticks([0, 2, 4, 6, 8, 10, 12])
 plt.show()
 
 plt.savefig('plots.png')
+
+# Bar plots and Histograms
+# Bar plots
+plt.bar([11, 14, 19, 13, 16], [0.2, 0.4, 0.6, 0.8, 1.0])
+plt.xticks(np.arange(11, 20))
+plt.title('Bar Graph Example')
+plt.show()
+
+# Histogram
+IQ = [85, 92, 115, 121, 72, 102, 104, 99, 110, 120, 121, 122, 90, 115, 119, 92, 98, 110, 138]
+Age = [70, 80, 90, 100, 110, 120, 130, 140, 150]
+
+plt.hist(IQ, Age, histtype='bar', rwidth=0.8)
+plt.xlabel('IQ range')
+plt.ylabel('No. of Students')
+
+plt.title('Histogram Example')
+plt.legend()
+
+plt.show()
+
+# Scatter Plots
+# Get some random points
+x = np.random.randn(200)
+y = np.random.randn(200)
+
+plt.scatter(x, y, c='r')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Scatter Plot Example')
+
+plt.show()
+
+# Pie Chart
+Programming_Languages = ["c", "C++", "Java", "Python", "ruby", "php"]
+Usage = [10, 20, 40, 60, 20, 30]
+
+# Pie chart is oval by default
+# To make it a circle
+plt.axis('equal')
+
+plt.pie(Usage, labels=Programming_Languages, shadow=True)
+plt.title("Pie Chart Example")
+plt.show()
